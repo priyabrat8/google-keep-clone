9bd76fb3d7bcbbb446eaf04220a7b4409fcd8de8
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2021-06-24 16:01:57 +0530

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,59 @@
+const addButton = document.querySelector('#add');
+const upateLSData = () => {
+    const textAreaData = document.querySelectorAll('textarea');
+    const notes = new Array();
+
+    textAreaData.forEach((note) => {
+        return notes.push(note.value);
+    })
+    
+    localStorage.setItem('notes',JSON.stringify(notes));
+}
+const addNewNote = (text = '') => {
+    const note = document.createElement('div');
+    note.classList.add('note');
+    const htmlData = `
+    <div class="operation">
+            <button class="edit"> <i class="fas fa-edit"></i> </button>
+            <button class="delete"> <i class="fas fa-trash-alt"></i> </button>
+        </div>
+
+        <div class="main ${text ? "" : "hidden"} "> </div>
+        <textarea class="${text ? "hidden" : ""} "></textarea>
+        `;
+    note.insertAdjacentHTML('afterbegin',htmlData);
+    const editButton = note.querySelector('.edit');
+    const delButton = note.querySelector('.delete');
+    const mainDiv = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    delButton.addEventListener('click', () =>{
+     note.remove();
+     upateLSData();  
+    });
+    textArea.value = text;
+    mainDiv.innerHTML = text;
+
+    editButton.addEventListener('click', () => {
+        mainDiv.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+
+    textArea.addEventListener('change',(event) => {
+        const value = event.target.value;
+        mainDiv.innerHTML = value;
+        
+        upateLSData();
+    });
+
+
+    document.body.appendChild(note);
+}
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes){ 
+    notes.forEach((note) => addNewNote(note))
+}
+
+addButton.addEventListener('click', () => addNewNote());
